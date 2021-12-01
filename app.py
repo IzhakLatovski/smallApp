11f151ab6b6cd5a8be01bc391c4fe0be774df801
Izhak <izhaklatovski@gmail.com>
2021-12-01 12:06:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def index():
         return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True, host='0.0.0.0')
+    app.run(port=8080, debug=True, host='0.0.0.0')
